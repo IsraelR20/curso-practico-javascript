383afa7ae36da3edc65e5dda77b63074ddc1a65f
IsraelR20 <israelrojasvilca@gmail.com>
2022-04-09 22:12:48 -0500

    Escribiendo HTML desde JavaScript

--- descuentos.js
+++ descuentos.js
@@ -2,3 +2,16 @@
 function calcularPrecioConDescuento(precio,descuento){
     return precio-descuento;
 }
+
+function precioConDescuento(){
+    const inputPrice=document.getElementById("inputPrice");
+    const priceValue=inputPrice.value;
+
+    const inputDiscount=document.getElementById("inputDiscount");
+    const priceDiscount=inputDiscount.value;
+    
+    const preciofinal=calcularPrecioConDescuento(priceValue,priceDiscount);
+
+    const resultadoEnPantalla=document.getElementById("resultFinal");
+    resultadoEnPantalla.innerText="El precio final con el desuento es:$"+preciofinal;
+}
